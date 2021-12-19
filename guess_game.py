4f6082b08d887608116fcd806b4066d72bdcbed4
Surbhi Bhardwaj <60005796+sleepygirl1989@users.noreply.github.com>
2021-12-19 22:50:44 +0530

    Update guess_game.py

--- guess_game.py
+++ guess_game.py
@@ -2,8 +2,12 @@ import random
 
 number = random.randint(1,10)
 
+
 def guess_game():
+    count = 0
     while True:
+        count=count+1
+
         guess_number = int(input("Please guess the number between range 1 to 9 : > "))
 
         if guess_number == number:
@@ -15,6 +19,7 @@ def guess_game():
 
         game_status = input("Enter exit if you wish to exit this game else type something else : ")
         if game_status=="exit":
+            print(count)
             break
 
 guess_game()
